79a001ed762ea279e22f8b219179fd4f2139a52d
victor augusto <victor.augusto1@outlook.com>
2019-05-30 22:22:54 -0300

    solved koan number 5

---
+++ src/i_introduction/_6_Data_Classes/JavaCode6.kt
@@ -0,0 +1,14 @@
+package i_introduction._6_Data_Classes;
+
+class JavaCode6{
+
+    class Person(name: String, age: Integer){
+        var name: String = ""
+        var age: Int = 0
+
+        fun getName(): String = name
+
+        fun getAge(): Integer = age
+        }
+    }
+}
